0961f0fd4fc0c791202b2b4835312343cf508355
Thiago Soares <thiago.soares.seraos@gmail.com>
2026-05-17 12:23:43 -0300

    Teste actions

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,7 +3,7 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 
 load_dotenv()
-
+#teste
 supabase: Client = create_client(
     os.getenv("SUPABASE_URL"),
     os.getenv("SUPABASE_ANON_KEY")
